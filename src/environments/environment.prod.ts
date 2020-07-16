36b690ce7920b2889a1effb30b2cfd79c76bc45d
al <mpustaka@gmail.com>
2020-07-16 16:06:58 +0700

    fix env prod api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -10,7 +10,7 @@ export const environment = {
   production: true,
   hmr: false,
   version: env.npm_package_version,
-  serverUrl: 'https://pokeapi.co/api/v2/',
+  serverUrl: 'https://api.pokemontcg.io/v1/',
   defaultLanguage: 'en-US',
   supportedLanguages: ['en-US'],
 };
